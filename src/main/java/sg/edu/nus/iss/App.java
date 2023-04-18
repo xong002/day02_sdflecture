b0723d1b201b3b3b12f520ac19f8caa36e24c1e7
Xin Zhi <115723006+xong002@users.noreply.github.com>
2023-04-18 09:44:41 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
